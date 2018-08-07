cf3fce069e6aaff4cd53f3975e7e30d8efe90f35
BritishPotato <denizhanak@gmail.com>
2018-08-07 13:18:40 +0300

    Add base board initialization

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ An AI made from the principles which guarantee as much success as possible.
 
 Construct internal data representation of game
 Formalize rules of game
-Formalize strategy to win game:
+Formalize strategy to win game
     Choose starting locations anywhere other than middle
     For now choose just 4. If blank space is not found, continue pressing.
     
@@ -29,7 +29,31 @@ Process data into internal data representation
 Use Probability Theory to make wise choice
 
 """
-# Strategy
+# %% Internal Representation of Game
+
+# Board constants
+board = []
+row_count = 8
+col_count = 8
+
+# Construct base board
+for i in range(row_count):
+    board.append("?" * col_count)
+
+# Function to print board
+def print_board(board):
+    for row in board:
+        print((" ".join(row)))
+        
+print_board(board)
+
+
+def make_move
+
+# Formalized Rules of Game
+
+
+# Formalized Strategy
 
 
 
