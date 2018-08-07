d288d561f2fbebbf07d703b672349a07385a183e
BritishPotato <denizhanak@gmail.com>
2018-08-08 00:10:21 +0300

    Prepare for seperation of files.

---
+++ representation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 00:08:29 2018
+
+@author: denizhan
+"""
+
---
+++ rules.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 00:09:03 2018
+
+@author: denizhan
+"""
+
