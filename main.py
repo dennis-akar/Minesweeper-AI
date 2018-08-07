adcac4b897d06bc4dfacf7513d0b1fdb2541b7a2
BritishPotato <denizhanak@gmail.com>
2018-08-07 12:59:53 +0300

    Add main file.

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  7 12:29:53 2018
+
+@author: Denizhan Akar
+
+Description
+An AI made from the principles which guarantee as much success as possible.
+
+@TODO, by priority
+
+Construct internal data representation of game
+Formalize rules of game
+Formalize strategy to win game:
+    Choose starting locations anywhere other than middle
+    For now choose just 4. If blank space is not found, continue pressing.
+    
+    Only consider empty tiles 
+    
+    If the number of empty tiles around warning number is equal to the 
+    warning number minus bomb tiles flagged around it, they are bombs.
+    
+    If the 
+
+Use OpenCV to take minesweeper game data
+Process data into internal data representation
+
+Use Probability Theory to make wise choice
+
+"""
+# Strategy
+
+
+
+
+
+
+
+
+
+
+
+
